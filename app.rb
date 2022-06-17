17b2a4b5e00630dccd04133d254c136de8eb4342
Magyar-Hunor Tamas <hunor@makers.tech>
2022-06-17 22:32:59 +0100

    Add routes to app.rb for now

--- app.rb
+++ app.rb
@@ -3,3 +3,55 @@ require 'sinatra'
 get '/' do
     'hello'
 end
+
+configure :development do
+    set :database, {
+        adapter: 'postgresql',
+        database: ENV['database'],
+        username: ENV['user'],
+        password: ENV['password']
+    }
+end
+
+get '/api/cars' do
+    @cars = Car.all
+    @cars.to_json
+end
+
+post '/api/cars' do
+    data = JSON.parse request.body.read  
+    @car = Car.create(
+        make: data['make'],
+        model: data['model'],
+        fuel_type: data['fuel_type'],
+        gearbox: data['gearbox'],
+        year: data['year']
+    )
+   @car.save
+   @car.to_json
+end
+
+get "/api/cars/:id" do
+    @car = Car.find(params[:id])
+    @car.to_json
+end
+
+put "/api/cars/:id" do
+    data = JSON.parse request.body.read  
+    @car = Car.find(params[:id])
+    @car.update( 
+        make: data['make'],
+        model: data['model'],
+        fuel_type: data['fuel_type'],
+        gearbox: data['gearbox'],
+        year: data['year']
+    )
+    @car.save
+    @car.to_json
+end    
+
+delete "/api/cars/:id" do
+    @car = Car.find(params[:id])
+    @car.destroy
+    @car.to_json
+end
